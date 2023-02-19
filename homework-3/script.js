aca1f84bcb7065c15e1ac75d18491ab0e7abc2de
smrus <118106745+smrus@users.noreply.github.com>
2023-02-19 14:47:01 +0300

    Add files via upload

---
+++ homework-3/script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+
+const money = 1000 //Nuber;
+console.log (typeof money);
+const profit = Фриланс // String;
+console.log (typeof profit);
+const expenses = "питание,проезд" //String;
+console.log ("длина" + expenses.length);
+const purpose = 2000 //Nuber;
+const period = 10 //Nuber;
+console.log ("накопить сумму за {period} месяцев");
+console.log ("цель заработать {purpose} доларов");
+let budgetDay = (money / 30);
+console.log (budgetDay);
+
